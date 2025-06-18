833a433b98c8d576654315364cbcbb5d102d6d07
IsWladi <wladimir2000@protonmail.com>
2025-06-18 18:56:40 -0400

    wip: migracion neovim 0.11.x;desinstala lsp-zero

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,86 +2,58 @@ return{
 	  --lsp para autocompletado
   {'neovim/nvim-lspconfig',
     dependencies = {
-      {'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        dependencies = {
-          -- LSP Support
-          {'williamboman/mason.nvim'},
-          {'williamboman/mason-lspconfig.nvim'},
+      -- LSP Support
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
 
-          -- Autocompletion
-          {'hrsh7th/nvim-cmp',
-          dependencies = {
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'hrsh7th/cmp-cmdline'},
-            {'petertriho/cmp-git'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
-          }
-        },
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp',
+        dependencies = {
+          {'hrsh7th/cmp-buffer'},
+          {'hrsh7th/cmp-path'},
+          {'hrsh7th/cmp-cmdline'},
+          {'petertriho/cmp-git'},
+          {'saadparwaiz1/cmp_luasnip'},
+          {'hrsh7th/cmp-nvim-lsp'},
+          {'hrsh7th/cmp-nvim-lua'},
         }
       },
     },
     config = function()
-      local lsp = require("lsp-zero")
+      require('mason').setup()
 
-      lsp.preset("recommended")
+      require('mason-lspconfig').setup{
+        ensure_installed = {
+          'lua_ls','ts_ls','jedi_language_server',
+          'tailwindcss','jsonls','dockerls','yamlls','html',
+          'rust_analyzer',
+        },
+        automatic_enable = { exclude = { 'jdtls' } },
+      }
 
-      lsp.ensure_installed({
-        'tsserver',
-        'jedi_language_server', -- apt install python3.10-venv
-        'tailwindcss',
-        'lua_ls',
-        'jsonls', -- en prueba
-        'dockerls', -- funciona bien, corrige sintaxis
-        'yamlls',
-        'html',
-        "jdtls",
-        "rust_analyzer"
+      local caps = require('cmp_nvim_lsp').default_capabilities()
+
+      -- 4. Overrides específicos
+      vim.lsp.config('lua_ls', {
+        capabilities = caps,
+        settings = { Lua = { diagnostics = { globals = { 'vim' } } } },
       })
 
-
-    local lspconfig = require('lspconfig')
-    local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-    -- Call setup on each LSP server
-    require('mason-lspconfig').setup_handlers({
-      function(server_name)
-        -- Don't call setup for JDTLS Java LSP because it will be setup from a separate config
-        if server_name ~= 'jdtls' then
-          lspconfig[server_name].setup({
-            -- on_attach = lsp_attach,
-            capabilities = lsp_capabilities,
-          })
-        end
-      end
-    })
-
-      lsp.configure('lua-language-server', {
-          settings = {
-              Lua = {
-                  diagnostics = {
-                      globals = { 'vim' }
-                  }
-              }
-          }
+      vim.lsp.config('tailwindcss', {
+        capabilities = caps,
+        filetypes = { 'css' },
       })
 
-      lsp.configure('tailwindcss', {
-        filetypes = { 'css' }
-      })
+      local lspconfig = require('lspconfig')
+      local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       local cmp          = require('cmp')
       local cmp_select   = {behavior = cmp.SelectBehavior.Select}
-      local cmp_mappings = lsp.defaults.cmp_mappings({
+      local cmp_mappings = cmp.mapping.preset.insert({
         ['<C-o>']        = cmp.mapping.select_prev_item(cmp_select),
         ['<C-e>']        = cmp.mapping.select_next_item(cmp_select),
         ['<C-a>']        = cmp.mapping.confirm({ select = true }),
         -- desactivar comportamientos indeseados
-        ['<Tab>']   = function(fallback) fallback() end,
-        ['<S-Tab>'] = function(fallback) fallback() end,
         ['<CR>']    = function(fallback) fallback() end,
         ['<Up>']    = function(fallback) fallback() end,
         ['<Down>']  = function(fallback) fallback() end,
@@ -92,24 +64,7 @@ return{
       cmp_mappings['<Tab>']   = nil
       cmp_mappings['<S-Tab>'] = nil
 
-      -- Fix mappings for luasnip with dvorak movement keys
-      _G.is_snippet_running = false -- flag to check if a snippet is running
-      local utils = require('utils')
-      cmp.event:on('confirm_done', function() -- when a completion is confirmed: disable rtns remap
-        utils.disable_rtns_mappings()
-        _G.is_snippet_running = true -- it means that a snippet is running or a completion is confirmed
-      end)
-      -- when ESC is pressed: enable rtns remap
-      vim.keymap.set({'i', 'v'}, '<Esc>', function ()
-          if _G.is_snippet_running then -- only call the function when is necessary
-              _G.is_snippet_running = false
-              utils.enable_rtns_mappings()
-          end
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
-      end, { noremap = true, silent = false })
-      -- Fix mappings for luasnip with dvorak movement keys
-
-      lsp.setup_nvim_cmp({
+      cmp.setup({
         mapping = cmp_mappings,
         sources = {
           {name = 'nvim_lsp'},
@@ -119,13 +74,27 @@ return{
           {name = 'nvim_lua'},
           {name = 'buffer'},
         },
-        -- Enable luasnip to handle snippet expansion for nvim-cmp
         snippet = {
-          expand = function(args)
-            require ( "luasnip").lsp_expand(args.body)
-          end,
+          expand = function(args) require('luasnip').lsp_expand(args.body) end,
         },
       })
+
+      -- Fix mappings for luasnip with dvorak movement keys
+      _G.is_snippet_running = false -- flag to check if a snippet is running
+      local utils = require('utils')
+      cmp.event:on('confirm_done', function() -- when a completion is confirmed: disable rtns remap
+        utils.disable_rtns_mappings()
+        _G.is_snippet_running = true -- it means that a snippet is running or a completion is confirmed
+      end)
+      -- when ESC is pressed: enable rtns remap
+      vim.keymap.set({'i', 'v'}, '<Esc>', function ()
+        if _G.is_snippet_running then -- only call the function when is necessary
+          _G.is_snippet_running = false
+          utils.enable_rtns_mappings()
+        end
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+      end, { noremap = true, silent = false })
+      -- Fix mappings for luasnip with dvorak movement keys
 
       -- Set configuration for specific filetype.
       cmp.setup.filetype('gitcommit', {
@@ -158,17 +127,8 @@ return{
           })
       })
 
-      lsp.set_preferences({
-          suggest_lsp_servers = false,
-          sign_icons          = {
-              error           = 'E',
-              warn            = 'W',
-              hint            = 'H',
-              info            = 'I'
-          }
-      })
-
-      lsp.on_attach(function(client, bufnr)
+      -- on_attach + keymaps ------------
+      local function my_on_attach(_, bufnr)
         local opts = {buffer = bufnr, remap = false}
         local hover = "[LSP] Hover"
         local diagnostic = "[LSP] Diagnostic"
@@ -186,13 +146,20 @@ return{
         vim.keymap.set("n", "<CR>e", function() vim.diagnostic.goto_next() end, {buffer = 0, remap = false, desc = diagnostic})
         vim.keymap.set("n", "<CR>r", function() vim.lsp.buf.rename() end, {buffer = 0, remap = false, desc = rename})
         vim.keymap.set("n", "<CR>ca", function() vim.lsp.buf.code_action() end, {buffer = 0, remap = false, desc = code_action})
-      end)
+      end
 
-      lsp.setup()
+      vim.api.nvim_create_autocmd('LspAttach', {
+        callback = function(args)
+          my_on_attach(vim.lsp.get_client_by_id(args.data.client_id), args.buf)
+        end,
+      })
 
       vim.diagnostic.config({
-          virtual_text = true
-})
+        virtual_text = true,
+        signs = {
+          text = { error = 'E', warn = 'W', hint = 'H', info = 'I' },
+        },
+      })
     end
   },
 }
