c1f47a50b92a4c6d9bda235a5a447f3bbcd76238
IsWladi <wladimir2000@protonmail.com>
2025-08-16 20:29:36 -0400

    feat(keymaps): optimize buffer navigation

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -3,29 +3,28 @@ local opt = vim.o
 --Configuración general
 
 -- opt.statuscolumn = "%@SignCb@%s%=%T%@NumCb@%l %r│%T "
-opt.number         = true
+opt.number = true
 opt.relativenumber = true
-opt.mouse          = 'a'
-opt.ignorecase     = true
-opt.smartcase      = true
-opt.breakindent    = true
-opt.expandtab      = true
-opt.tabstop        = 4
-opt.softtabstop    = 4
-opt.shiftwidth     = 4
-opt.smartindent    = true
-opt.autoindent     = true
-opt.encoding       = "utf-8"
-opt.clipboard      = "unnamedplus"
-opt.swapfile       = false
-opt.termguicolors  = true
-opt.hlsearch       = true --resaltado de busqueda con /palabra
-opt.incsearch      = true --resaltado de busqueda mientras se escribe con h1search false
-opt.colorcolumn    = "120" --columna de marco para no exceder largo de linea
-opt.undofile       = true
+opt.mouse = "a"
+opt.ignorecase = true
+opt.smartcase = true
+opt.breakindent = true
+opt.expandtab = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.smartindent = true
+opt.autoindent = true
+opt.encoding = "utf-8"
+opt.clipboard = "unnamedplus"
+opt.swapfile = false
+opt.termguicolors = true
+opt.hlsearch = true --resaltado de busqueda con /palabra
+opt.incsearch = true --resaltado de busqueda mientras se escribe con h1search false
+opt.colorcolumn = "120" --columna de marco para no exceder largo de linea
+opt.undofile = true
 local home = os.getenv("HOME") or os.getenv("USERPROFILE")
 opt.undodir = home .. "/.vim/undodir"
-opt.timeoutlen = 500 -- tiempo de espera para teclas mapeadas
 vim.opt.laststatus = 3 -- recomendacion plugin Avante.nvim
 
 --auto comandos
@@ -33,10 +32,10 @@ vim.cmd("autocmd BufReadPost * lua require('wrap').set_wrap()") -- auto wrap
 
 -- auto delete whitespaces at the end of line, end of file, beginning of file and preserv cursor
 function delete_blank_lines()
-    local first_line = vim.fn.getline(1)
-    if first_line == "" then
-        vim.cmd([[1;/\S/-d]])
-    end
+	local first_line = vim.fn.getline(1)
+	if first_line == "" then
+		vim.cmd([[1;/\S/-d]])
+	end
 end
 
 vim.cmd([[
@@ -51,7 +50,5 @@ vim.cmd([[
   autocmd BufWritePre * silent! call PreserveCursor()
 ]])
 
-
 -- Restaurar la posición del cursor y centrar pantalla
 vim.cmd('autocmd BufReadPost * silent! normal! g`"zvzz')
-
