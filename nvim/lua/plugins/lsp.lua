2ee9534f1fe4059ff513f8b5d23ae0242f8b7604
IsWladi <wladimir2000@protonmail.com>
2025-11-09 13:49:47 -0300

    fix: agrega cmp-nvim-lsp para usar jdtls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -6,6 +6,7 @@ return {
 			-- LSP Support
 			{ "williamboman/mason.nvim" },
 			{ "williamboman/mason-lspconfig.nvim" },
+			{ "hrsh7th/cmp-nvim-lsp" },
 		},
 		config = function()
 			require("mason").setup()
